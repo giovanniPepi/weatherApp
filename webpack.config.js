cd056f37ae68de11500d51d37fb6ac75a753cd7b
gtpepi <gtpepi@hotmail.com>
2022-05-25 20:55:09 -0300

    Add initial structure

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(ttf)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.svg$/i,
+        loader: 'svg-inline-loader'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
+  }
+};
