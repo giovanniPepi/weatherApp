b4dc1fa2dd5461eafc4bed739ce4c772ddabe191
gtpepi <gtpepi@hotmail.com>
2022-05-27 22:43:31 -0300

    Add humidity and UV svgs

--- src/getStyle.js
+++ src/getStyle.js
@@ -1,8 +1,16 @@
 import searchIcon from './img/icons/magnify.svg';
+import humidityIcon from './img/icons/humidity-svgrepo-com.svg';
+import uvIcon from './img/icons/sun-svgrepo-com.svg';
 
 const getStyle = () => {
   const goBtn = document.querySelector('.go');
   goBtn.innerHTML = searchIcon;
+
+  const humiditySvg = document.querySelector('.humiditySvg');
+  humiditySvg.innerHTML = humidityIcon;
+
+  const uvSvg = document.querySelector('.uvSvg');
+  uvSvg.innerHTML = uvIcon;
 };
 
 export default getStyle;
